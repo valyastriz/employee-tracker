b063c8fd7f9fa9190b36ac142b8fac4fa94852e4
valyastriz <valyastriz@gmail.com>
2024-07-29 17:29:33 -0400

    added function to add a new employee

--- src/models/employee.js
+++ src/models/employee.js
@@ -57,4 +57,35 @@ async function viewAllEmployees() {
     }
 }
 
+// function to add a new employee
+async function addEmployee(first_name, last_name, role_name, manager_name) {
+    try {
+        const role = await Role.findOne({ where: {title: role_name }});
+        if (!role) throw new Error(`Role with title "${role_name}" not found`);
+
+        let manager = null;
+        if (manager_name) {
+            manager = await Employee.findOne({
+                where: sequelize.where(
+                    sequelize.fn('concat', sequelize.col('first_name'), ' ', sequelize.col('last_name')), manager_name
+                ),
+            });
+            if (!manager) throw new Error(`Manager with name "${manager_name}" not found.`);
+        }
+
+        const employee = await Employee.create({
+            first_name,
+            last_name,
+            role_id: role.id,
+            manager_id: manager ? manager.id : null,
+        });
+
+        console.log('Employee added: ', employee.get({ plain: true}));
+        return employee;
+    } catch (err) {
+        console.error('Error executing query', err.stack);
+    }
+}
+
+
 module.exports = Employee;
