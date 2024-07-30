d38f3ae4ae2e5b7699626de534c6e3bd629bf46e
valyastriz <valyastriz@gmail.com>
2024-07-29 22:13:46 -0400

    updated connection.js file

--- src/config/connection.js
+++ src/config/connection.js
@@ -1,17 +1,21 @@
 // will handle the connection to the porstresql database
 
 require('dotenv').config();
+const { Sequelize } = require('sequelize');
 
-const Sequelize = require('sequelize');
+const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+        host: process.env.DB_HOST,
+        dialect: 'postgres',
+        port: process.env.DB_PORT,
+        dialectOptions: {
+            decimalNumbers: true,
+        },
+    }
+);
 
-const sequelize = process.env.DB_URL
-  ? new Sequelize(process.env.DB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-      host: 'localhost',
-      dialect: 'postgres',
-      dialectOptions: {
-        decimalNumbers: true,
-      },
-    });
 
 module.exports = sequelize;
